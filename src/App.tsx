90f96c64dd0b66908871a6c7964a43a3fbd7aaa7
chetana.lotliker <chetanalotliker@gmail.com>
2024-01-26 22:19:07 +0530

    Create useState future value

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Input } from './components/Input';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
+import { User } from './components/state/User';
 
 function App() {
   const personName = {
@@ -46,6 +47,7 @@ function App() {
       <Input value='' handleChange={(event)=> console.log(event)}/>
       <br/><br/>
       <Container styles={{ border:'1px solid black', padding:'1rem'}}/>
+      <User />
     </div>
   );
 }
